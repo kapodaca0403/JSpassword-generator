6d0880215126b3b72636065a7aa1cb1dfd235dbf
katerz avocado <ktrinaapodaca@yahoo.com>
2021-08-03 02:42:37 -0600

    adding changes

--- script.js
+++ script.js
@@ -14,26 +14,53 @@ var passwordOptions = {
   lowercase: true, 
   uppercase: true, 
   numeric: true, 
-  specialCharacters: true
+  specialCharacters: true,
 }
+
+
+var lowercase = ["abcdefghijklmnopqrstuvwxyz"];
+var uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var numeric = ["0123456789"]; 
+var specialCharacters = [" !#$%&'()*+,-./:;<=>?@]\[^_`{|}~ "];
+
+
+
 
 function getLength() {
   var length = prompt ("How long do you want your password to be? Enter a number between 8 and 128");
-console.log(length);
+  for ( var i = 0 ; i < length.length; i++) // how do i make sure the user is only picking out of those options , no less than 8 and no more than 128
+  console.log(length);
 }
+
 function getConditions(){
-  alert ("Please select at least one of the following: lowercase, uppercase, numeric and special character");
-passwordOptions.lowercase = confirm ("Would you like to use lowercase letters?"); 
-}
+  alert ("Please select at least one of the following: lowercase, uppercase, numeric or special character"); // this is an alert to let the user know the criteria 
+
+  passwordOptions.lowercase = confirm ("Would you like to use lowercase letters?"); 
+
+  if (passwordOptions.lowercase == true) // how do i get this to move to the next prompt after selecting a criteria or making sure a criteria is picked 
+
+
+ if ( uppercpasswordOptions.uppercase = confirm ("Would you like to add uppercase?");
+ 
+ if (numeric) passwordOptions.numeric = confirm ("Would you like to add a number?");
+
+else  (specialCharacters)
+  passwordOptions.specialCharacters = confirm ("Would you like to add a special character?"); 
+   
+  console.log(getConditions); 
 
 
 
-// variable declaration 
+ // i need to be able to get random characters for the criteria and make a random password with the requests from the user
+
+
+
+//variable declaration 
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  getPassword();
+  getLength();
   getConditions();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -41,6 +68,3 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
